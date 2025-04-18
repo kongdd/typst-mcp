c46f7017c3b71ea1205b4a302f9f8a2d440fc442
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2025-04-18 14:03:37 +0200

    get chapter resource

--- server.py
+++ server.py
@@ -40,5 +40,44 @@ def list_chapters() -> str:
         chapters += list_child_routes(chapter)
     return json.dumps(chapters)
 
+@mcp.resource("docs://chapters/{route}")
+def get_chapter(route: str) -> str:
+    """
+    Gets a chapter by route.
+    The route is the path to the chapter in the typst docs.
+    For example, the route "____reference____layout____colbreak" corresponds to the chapter "reference/layout/colbreak".
+    The route is a string with underscores ("____") instead of slashes (because MCP uses slashes to separate the input parameters).
+    """
+    print(f"mcp.resource('docs://chapters/{route}') called")
+
+    # the rout could also be in the form of "____reference____layout____colbreak" -> "/reference/layout/colbreak"
+    # replace all underscores with slashes
+    route = route.replace("____", "/")
+
+    def route_matches(chapter_route: str, input_route: str) -> bool:
+        return chapter_route.strip("/") == input_route.strip("/")
+
+    def get_child(chapter: dict, route: str) -> dict:
+        """
+        Gets a child chapter by route.
+        """
+        if "children" not in chapter:
+            return {}
+        for child in chapter["children"]:
+            if route_matches(child["route"], route):
+                return child
+            child = get_child(child, route)
+            if child:
+                return child
+        return {}
+    for chapter in typst_docs:
+        if route_matches(chapter["route"], route):
+            return json.dumps(chapter)
+        child = get_child(chapter, route)
+        if child:
+            return json.dumps(child)
+    return json.dumps({})
+
 if __name__ == "__main__":
-    print(json.dumps(list_chapters(), indent=2))
+    # print(json.dumps(list_chapters(), indent=2))
+    # print(json.dumps(get_chapter("____reference____layout____colbreak"), indent=2))
