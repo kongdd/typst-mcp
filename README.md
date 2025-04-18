bce651ebd305f4edae58f9d2db09a0267d085892
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2025-04-18 13:31:32 +0200

    first resource (list doc chapters)

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
   - `cargo run --package typst-docs -- --assets-dir ../typst-mcp/typst-docs --out-file ../typst-mcp/typst-docs/main.json`
     - make sure to adjust the path to your local clone of the typst-mcp repository
     - this will generate the `main.json` and the assets in the `typst-docs` folder
+
+## JSON Schema of the Typst Documentation
+
+>⚠️ The schema of the typst documentation is not stable and may change at any time. The schema is generated from the typst source code and is not guaranteed to be complete or correct. Use at your own risk.
--- server.py
+++ server.py
@@ -1,19 +1,44 @@
-# server.py
 from mcp.server.fastmcp import FastMCP
+import json
 
-# Create an MCP server
 mcp = FastMCP("Demo")
 
+# load the typst docs JSON file
+raw_typst_docs = ""
+with open("typst-docs/main.json", "r") as f:
+    raw_typst_docs = f.read()
+typst_docs = json.loads(raw_typst_docs)
 
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+@mcp.resource("docs://chapters")
+def list_chapters() -> str:
+    """
+    Lists all chapters in the typst docs.
+    The LLM should use this in the beginning to get the list of chapters and then decide which chapter to read.
+    """
+    print("mcp.resource('docs://chapters') called")
+    def list_child_routes(chapter: dict) -> list[dict]:
+        """
+        Lists all child routes of a chapter.
+        """
+        if "children" not in chapter:
+            return []
+        child_routes = [] # { "route": str, content_length: int }[]
+        for child in chapter["children"]:
+            if "route" in child:
+                child_routes.append({
+                    "route": child["route"],
+                    "content_length": len(json.dumps(child))
+                })
+            child_routes += list_child_routes(child)
+        return child_routes
+    chapters = []
+    for chapter in typst_docs:
+        chapters.append({
+            "route": chapter["route"],
+            "content_length": len(json.dumps(chapter))
+        })
+        chapters += list_child_routes(chapter)
+    return json.dumps(chapters)
 
-
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+if __name__ == "__main__":
+    print(json.dumps(list_chapters(), indent=2))
